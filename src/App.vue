9771ec73e7fa408bc16d3d10dc7d5a370dc4a4ad
Fabio <alessf13@gmail.com>
2021-09-12 19:14:50 +0200

    feat: project setup and phone component created

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Phone />
+</template>
+
+<script>
+import Phone from "./components/Phone.vue";
+
+export default {
+  name: "App",
+  components: {
+    Phone,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+</style>
